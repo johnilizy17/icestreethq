2fa05df50a84a60df4a9903e4d813e34699fb461
johnilizy17 <johnabrahamtosin@gmail.com>
2024-10-29 14:55:26 +0100

    image control

--- components/homepagecomponents/SingleItem/index.tsx
+++ components/homepagecomponents/SingleItem/index.tsx
@@ -59,9 +59,9 @@ export default function SingleItem({ title, category, type, createdBy, label, pr
                             <Box mt="30px" paddingLeft={["30px"]} flexWrap="wrap" display="flex">
                                 {data?.map((item: any, index: number) => {
                                     return (
-                                        <Box mb="20px" mr="20px" _hover={{ transform: "scale(1.05)", transition: "0.5s ease-in-out" }} cursor="pointer" onClick={() => clickHandler(item._id)} key={index} onMouseOver={() => setIsHover(index)} onMouseOut={() => setIsHover(-1)} role="button" w="300px" h="418px" >
+                                        <Box mb="20px" mr="20px" _hover={{ transform: "scale(1.05)", transition: "0.5s ease-in-out" }} cursor="pointer" onClick={() => clickHandler(item._id)} key={index} onMouseOver={() => setIsHover(index)} onMouseOut={() => setIsHover(-1)} role="button" w="300px">
                                             <Center h={["310px", "310px"]} borderRadius={["8px","16px","24px"]} overflow="hidden">
-                                                <Img src={imagePath + "/" + item?.image} objectFit="cover" alt={item?.name} />
+                                                <Img h="100%" src={imagePath + "/" + item?.image} objectFit="cover" alt={item?.name} />
                                             </Center>
                                             <Box>
                                                 <Flex mt="20px" justifyContent="space-between">
