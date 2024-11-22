292f61808166cf400530e19ad0dba46c5cc31980
johnilizy17 <johnabrahamtosin@gmail.com>
2024-11-22 05:21:36 +0100

    payment page

--- components/Payment/index.js
+++ components/Payment/index.js
@@ -6,9 +6,46 @@ export default function PaymentCheckout({ paymentSuccessfull, userDetails, SumTo
 
     const toast = useToast();
     // you can call this function anything
+    const handlePaystackSuccessAction = (reference) => {
+        toast({
+            title: "Payment",
+            description: "Payment process wait a minute to complete the process",
+            position: "top-right",
+            status: "success",
+            isClosable: true,
+        })
+        paymentSuccessfull()
+    };
+
+    // Config setting
+    const config = {
+        reference: (new Date()).getTime().toString(),
+        email: userDetails && userDetails?.user ? userDetails?.user.email : "johnabrahamtosin@gmail.com",
+        amount: SumTotalFunction, //Amount is in the country's lowest currency. E.g Kobo, so 20000 kobo = N200
+        publicKey: 'pk_test_05010684d9dbcd82b2b931fa886f9b01904e5d78',
+    };
+
+    // you can call this function anything
+    const handlePaystackCloseAction = (e) => {
+        // implementation for  whatever you want to do when the Paystack dialog closed.
+        toast({
+            title: "Payment",
+            description: "Payment cancel by user",
+            position: "top-right",
+            status: "error",
+            isClosable: true,
+        })
+    }
+
+    const componentProps = {
+        ...config,
+        text: '',
+        onSuccess: (reference) => handlePaystackSuccessAction(reference),
+        onClose: handlePaystackCloseAction,
+    };
 
     return (
-        <Box >
+        <Box pos="relative">
             <Center cursor="pointer" p="10px" justifyContent="space-between" bg="whitesmoke" borderRadius={"14px"}>
                 <Center>
                     <Img h="40px" src="/images/download.png" />
@@ -19,6 +56,7 @@ export default function PaymentCheckout({ paymentSuccessfull, userDetails, SumTo
 
                 <Radio colorScheme='black' isChecked={false} value='2'></Radio>
             </Center>
+            <PaystackButton className="paystack-button" {...componentProps} />
         </Box>
     );
 }
