95cf087c98381ddbaf4bdbddf10aa497fd894a5c
johnilizy17 <johnabrahamtosin@gmail.com>
2024-11-26 22:47:53 +0100

    new changes

--- services/publicConfig.ts
+++ services/publicConfig.ts
@@ -2,10 +2,26 @@ import axios from "axios";
 let baseURL = "https://api.icestreethq.com/";
 let token;
 
+if (typeof window !== 'undefined') {
+  // Perform localStorage action
+  token = localStorage.getItem("token");
+}
 axios.defaults.headers.common["Content-Type"] = "application/x-www-form-urlencoded";
 
+if (token) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+}
 const instancePublic = axios.create({
   baseURL,
 });
+
+
+export const flutterWaveConfig = axios.create({
+  baseURL: "https://api.flutterwave.com/v3",
+  headers: {
+    "Content-Type": 'application/json',
+    "Authorization": `Bearer null`
+  }
+})
 
 export default instancePublic;
