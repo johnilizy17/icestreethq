e3a47f5cc8e132f30e391ff4c5fbe2b5c720a366
johnilizy17 <johnabrahamtosin@gmail.com>
2024-12-07 17:37:13 +0100

    scroll bar

--- pages/size/index.tsx
+++ pages/size/index.tsx
@@ -1,10 +1,13 @@
+import { Box } from "@chakra-ui/react";
 import MenuLayout from "../../components/MenuLayout";
 import SizeGuide from "../../components/SizeGuide";
 
 export default function Home() {
     return (
         <MenuLayout pageName='Ice Street' menu={false} category={false}>
+            <Box overflow="scroll">
             <SizeGuide />
+            </Box>
         </MenuLayout>
     );
 }
