efbe800f0d5b1613fc53e4861d7bfeea2d61b013
johnilizy17 <johnabrahamtosin@gmail.com>
2024-12-13 13:49:02 +0100

    package home

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,9 @@ import toast from "react-hot-toast";
 import HeroSearch from '../components/homepagecomponents/HeroSection/Search'
 import Subscription from '../components/homepagecomponents/Subscription'
 import AllCollection from '../components/AllCollection'
-import { Box, Center } from '@chakra-ui/react'
+import { Box, Center, Text } from '@chakra-ui/react'
 import { COLORS } from '../services/theme/colors'
+import Marquee from 'react-fast-marquee'
 
 export default function Home() {
 
@@ -35,16 +36,18 @@ export default function Home() {
       <main>
         <Center fontWeight="bold" w="full" h={["70px", "50px"]} pos="fixed" zIndex={1000} top="0px" background={COLORS.black} color={COLORS.white}>
           <Box textAlign={"center"}>
-            Welcome to Ice Street Official Store. Enjoy up to 50% Discount Season sales →
+          <Marquee>
+            Welcome to Ice Street Official Store. Enjoy up to 50% Discount Season sales →<Text style={{width:100}} />
+          </Marquee>
           </Box>
         </Center>
         <MenuLayout home={true} pageName='Ice Street' menu={false} category={false}>
-          <div className=' w-full mt-[10px] lg:mt-[32px] ' >
+          <Box mt={["90px", "80px"]} className=' w-full  lg:mt-[32px] ' >
             <HeroSection />
             {/* <HeroSearch /> */}
             {category?.map((item: any, index: number) => {
               return (
-                <div key={index} className=' w-full ' >
+                <Box key={index} className=' w-full ' >
                   {/* {item.title === "kids & Babies" &&
                   } */}
                   {item.category_id.style === 1 ?
@@ -52,13 +55,13 @@ export default function Home() {
                     :
                     <SingleItem label={false} type={item.category_id} category={item?.category_id._id} createdBy={item.category_id._id} product={item.product_id} />
                   }
-                </div>
+                </Box>
 
               )
             })}
 
             <AllCollection label={false} />
-          </div>
+          </Box>
           <Subscription />
         </MenuLayout>
       </main>
