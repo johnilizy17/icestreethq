0a06f3350630373edd0f07052259b542e4656b6c
johnilizy17 <johnabrahamtosin@gmail.com>
2024-12-15 11:44:23 +0100

    new changes

--- pages/otp/index.tsx
+++ pages/otp/index.tsx
@@ -18,12 +18,13 @@ export default function ForgottenPassword() {
     const navigate = useRouter()
     const loginSchema = yup.object({
         otp: yup.string().required('Your OTP is required'),
+        email: yup.string().required('Your Email is required'),
         password: yup.string().required('Your Password is required')
     })
 
     // formik
     const formik = useFormik({
-        initialValues: { otp: '', password: '' },
+        initialValues: { otp: '', password: '', email:"" },
         validationSchema: loginSchema,
         onSubmit: () => { },
     });
@@ -75,6 +76,25 @@ export default function ForgottenPassword() {
                     <div className=' lg:loginShadow w-full lg:w-[600px] font-medium my-12 rounded-xl flex flex-col bg-white py-8 px-4 lg:px-[45px] ' >
                         <p className=' font-bold lg:text-2xl text-center ' >Reset Password</p>
                         <div className=' w-full mt-10 lg:mt-8 ' >
+                            <p className=' text-sm font-medium mb-2 ' >Email</p>
+                            <Input
+                                name="email"
+                                onChange={formik.handleChange}
+                                onFocus={() =>
+                                    formik.setFieldTouched("email", true, true)
+                                }
+                                height="45px" border="1px solid #595959E5" placeholder='Enter OTP' fontSize="sm" />
+                            <div className="w-full h-auto pt-2">
+                                {formik.touched.email && formik.errors.email && (
+                                    <motion.p
+                                        initial={{ y: -100, opacity: 0 }}
+                                        animate={{ y: 0, opacity: 1 }}
+                                        className="text-xs font-Inter-Medium text-[#ff0000]"
+                                    >
+                                        {formik.errors.email}
+                                    </motion.p>
+                                )}
+                            </div>
                             <p className=' text-sm font-medium mb-2 ' >OTP</p>
                             <Input
                                 name="otp"
